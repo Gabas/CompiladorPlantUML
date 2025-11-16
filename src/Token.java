49e3c11f9105bebaafda8647a20ee5f7b6b8fd12
Gabas <ast.gabriel2004@gmail.com>
2025-11-16 14:23:32 -0300

    tokens criados

--- src/Token.java
+++ src/Token.java
@@ -1,3 +1,43 @@
 public class Token {
     
+    public enum TipoToken {
+        // Palavras-chave
+        T_START_UML,
+        T_END_UML,
+        T_CLASS,
+        T_ABSTRACT,
+        T_STATIC,
+
+        // Símbolos
+        T_OPEN_BRACE,       // {
+        T_CLOSE_BRACE,      // }
+        T_OPEN_PAREN,       // (
+        T_CLOSE_PAREN,      // )
+        T_SEMICOLON,        // ;
+        T_COLON,            // :
+        T_COMMA,            // ,
+
+        // Visibilidade
+        T_PUBLIC,           // +
+        T_PRIVATE,          // -
+        T_PROTECTED,        // #
+        T_PACKAGE,          // ~
+
+        // Relacionamentos
+        T_HERANCA,          // <|--
+        T_IMPLEMENTACAO,    // <|..
+        T_ASSOCIACAO,       // -->
+        T_AGREGACAO,        // o--
+        T_COMPOSICAO,       // *--
+        T_LINK,             // --
+
+        // Identificadores e Literais
+        T_ID,               // Identificador
+        T_SRING_LITERAL,    // "texto"
+
+        // Controle
+        T_NEWLINE,          // Quebra de linha
+        T_EOF               // Fim do arquivo
+    }
+
 }
